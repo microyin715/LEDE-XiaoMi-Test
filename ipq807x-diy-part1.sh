23cf277b04d74133645590f90c7f4255e72506ed
microyin715 <66552744+microyin715@users.noreply.github.com>
2023-10-29 23:14:31 +0800

    Update ipq807x-diy-part1.sh

--- ipq807x-diy-part1.sh
+++ ipq807x-diy-part1.sh
@@ -19,5 +19,7 @@
 # 添加插件源码
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 # passwall依赖
-sed -i '$a src-git small https://github.com/microyin715/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# 添加small-package
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
