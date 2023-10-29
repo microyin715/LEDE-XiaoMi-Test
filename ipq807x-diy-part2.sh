af3becb714f2f8cd1ec012ac1ecc4e943627d5e8
microyin715 <66552744+microyin715@users.noreply.github.com>
2023-10-29 23:16:29 +0800

    Update ipq807x-diy-part2.sh

--- ipq807x-diy-part2.sh
+++ ipq807x-diy-part2.sh
@@ -63,7 +63,7 @@ git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-th
 
 #添加额外非必须软件包
 #git clone https://github.com/kenzok8/small-package package/small-package
-git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 #git clone https://github.com/garypang13/luci-app-bypass package/luci-app-bypass
 git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 #git clone https://github.com/jerrykuku/luci-app-jd-dailybonus.git package/luci-app-jd-dailybonus
@@ -79,6 +79,6 @@ git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 
 #git clone https://github.com/riverscn/openwrt-iptvhelper.git package/openwrt-iptvhelper
 #添加smartdns
-git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
+#git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
+#git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 #git clone -b luci https://github.com/pexcn/openwrt-chinadns-ng.git package/luci-app-chinadns-ng
