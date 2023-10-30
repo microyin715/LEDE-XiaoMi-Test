78ab0c2e773afd903c222d0236c2bc188a0d420a
microyin715 <66552744+microyin715@users.noreply.github.com>
2023-10-30 15:41:54 +0800

    Update ipq807x-diy-part1.sh

--- ipq807x-diy-part1.sh
+++ ipq807x-diy-part1.sh
@@ -16,12 +16,15 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
 # 添加插件源码
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+
 echo 'passwall依赖'
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # 添加small-package
-#sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+
 #echo '添加Passwall软件源'
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo '=========Add feed source OK!========='
